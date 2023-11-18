58dd1b1ca511cc64dce919647494be0329537e09
zakaria <zikokh012@gmail.com>
2023-11-18 01:46:40 +0100

    insersion dans les tables organisation evenement lieu

--- data.sql
+++ data.sql
@@ -30,7 +30,6 @@ CREATE TABLE evenement (
 
 
 -- creation de table organisation
-creation de table 
 CREATE TABLE organisation (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_evenement INT,
@@ -67,7 +66,7 @@ CREATE TABLE sponsor (
 
 
 
-creation de table 
+-- creation de table pub
 CREATE TABLE pub (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_evenement INT,
@@ -95,4 +94,73 @@ ADD FOREIGN KEY (id_participant) REFERENCES participant(id);
 
 
 
--- insertion de table 
+-- insertion de table participant
+
+INSERT INTO participant (name, email)
+VALUES 
+    ('hamza', 'hamza@gmail.com'),
+    ('khalid', 'khalid@gmail.com'),
+    ('yasser', 'yasser@gmail.com'),
+    ('adam', 'adam@gmail.com'),
+    ('ali', 'ali@gmail.com'),
+    ('khadijam', 'khadijam@gmail.com'),
+    ('siren', 'siren@gmail.com');
+
+
+
+-- insertion dans la table organisateur
+INSERT INTO organisateur
+VALUES ('BDE group', 'bdegroup@example.com'),
+('youcode', 'youcode@example.com'),
+('um6p','um6p@gmail.com'),
+('simplon','simplon@gmail.com');
+
+--  insert dans la table lieu
+INSERT INTO lieu (name, address)
+VALUES 
+    ('casa', 'bernoussi TARIQ'),
+    ('yousoufia', 'quatier mohamadi'),
+    ('rabat', 'agdal - rue france');
+
+
+-- insession dans la table evenement
+INSERT INTO evenement (name, date_debut, date_fin, id_organisateur, id_lieu)
+VALUES 
+    ('journey integration', '2023-11-25', '2023-11-28', 3, 1),
+    ('workshop', '2023-11-26', '2023-11-29', 3, 1),
+    ('hackathon', '2023-11-30', '2024-1-4', 2, 3),
+    ('workshop mysql', '2024-1-25', '2024-1-29', 2, 3),
+    ('DEVOX', '2024-2-12', '2024-2-18', 1, 2),
+    ('evenement de META', '2024-3-20', '2024-3-27', 2, 2),
+    ('huawei', '2024-4-2', '2024-4-4', 2, 3),
+    ('workshop java', '2024-4-22', '2024-4-25', 3, 1),
+    ('AI', '2024-5-2', '2024-5-3', 1, 2);
+
+
+
+-- insertion das la table organisation
+INSERT INTO organisation (id_evenement, id_organisateur)
+VALUES 
+    (2, 2), 
+    (1, 1), 
+    (2, 3),
+    (3, 3);
+
+
+
+-- insertion dans la table billet
+INSERT INTO billet (prix, type, id_evenement)
+VALUES 
+    (50.00, 'VIP', 1),
+    (30.00, 'Standard', 1),
+    (45.00, 'VIP', 1),
+    (25.00, 'Standard', 2),
+    (55.00, 'VIP', 2),
+    (35.00, 'Standard', 2),
+    (40.00, 'VIP', 1),
+    (28.00, 'Standard', 1),
+    (48.00, 'VIP', 2),
+    (32.00, 'Standard', 2);
+
+
+
